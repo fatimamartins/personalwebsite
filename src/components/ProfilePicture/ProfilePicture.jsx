d0597a726a875cee635d12e9e4d3adda1510becb
Fátima Martins <fatima.l.coelho.martins@gmail.com>
2019-07-14 16:20:29 +0200

    implemented ProfilePage

--- src/components/ProfilePicture/ProfilePicture.jsx
+++ src/components/ProfilePicture/ProfilePicture.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const ProfilePicture = () => {
-    return  <div className="profile-picture">
+const ProfilePicture = () => (
+    <div className="profile-picture">
         <div className="profile-picture--background">
             <img src={require("../../assets/img/perfil-backgroung.png")} alt="background"/>
         </div>
@@ -9,6 +9,6 @@ const ProfilePicture = () => {
             <img src={require("../../assets/img/perfil.png")} alt="Profile"></img>
         </div>
     </div>
-};
+);
 
 export default ProfilePicture;
