781c624f05618fabb3abc30da11556452df3d34c
Fátima Martins <fatima.l.coelho.martins@gmail.com>
2019-10-22 15:44:49 +0200

    update portfolio

--- src/components/Pages/PortfolioPage.jsx
+++ src/components/Pages/PortfolioPage.jsx
@@ -23,7 +23,7 @@ const PortfolioPage = () => (
                             <p>Member of the react team that implemented project with react</p>
                             <p>Setup continuous</p>
                         </div>
-                            <a href="#" target="_blank" className="btn btn--white">Visit us</a>
+                            <a href="#" target="_blank" className="btn btn--white">Check it out</a>
                     </div>
                 </div>
                 <div className="card">
@@ -39,7 +39,7 @@ const PortfolioPage = () => (
                             <p>Member of the react team that implemented project with react</p>
                             <p>Setup continuous</p>
                         </div>
-                        <a href="https://www.habitects.ch" target="_blank" className="btn btn--white">Visit us</a>
+                        <a href="https://www.habitects.ch" target="_blank" className="btn btn--white">Check it out</a>
                     </div>
                 </div>
             </div>
@@ -56,108 +56,89 @@ const PortfolioPage = () => (
                     </div>
                     <div className="card__side card__side--back card__side--back-1">
                         <div>
-                            <p>E-commerce platform specialized in bathroom accessories; </p>
-                            <p>Build first version oh the platform using HTML, CSS, jQuery and Bootstrap;</p>
-                            <p>Member of the react team that implemented project with react</p>
-                            <p>Setup continuous</p>
+                            <p>Landing page for a fictional company - Omnifood - a premium food delivery service</p>
+                            <p>Introduction to HTML5, CSS3 and responsive website design</p>
+                            <p>jQuery effects like animations and sticky navigation</p>
+                            <p>Web Design basic concepts</p>
                         </div>
-                            <a href="#" target="_blank" className="btn btn--white">Visit us</a>
+                            <a href="#" target="_blank" className="btn btn--white">Check it out</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="card__side card__side--front">
-                        <div className="card__picture card__picture-1">
-                            <h4 className="card__heading">One Tree</h4>
-                        </div>
-                    </div>
-                    <div className="card__side card__side--back card__side--back-2">
-                        <div>
-                            <p>E-commerce platform specialized in bathroom accessories; </p>
-                            <p>Build first version oh the platform using HTML, CSS, jQuery and Bootstrap;</p>
-                            <p>Member of the react team that implemented project with react</p>
-                            <p>Setup continuous</p>
-                        </div>
-                        <a href="#" target="_blank" className="btn btn--white">Visit us</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card__side card__side--front">
-                        <div className="card__picture card__picture-1">
+                        <div className="card__picture card__picture-pigGame">
                             <h4 className="card__heading">Pig Game</h4>
                         </div>
                     </div>
                     <div className="card__side card__side--back card__side--back-1">
                         <div>
-                            <p>E-commerce platform specialized in bathroom accessories; </p>
-                            <p>Build first version oh the platform using HTML, CSS, jQuery and Bootstrap;</p>
-                            <p>Member of the react team that implemented project with react</p>
-                            <p>Setup continuous</p>
+                            <div>
+                                <p>The Pig game is a simple dice game.</p>
+                                <a href="https://en.wikipedia.org/wiki/Pig_(dice_game)" target="_blank">Find more here!</a>
+                            </div>
+                            <p>Javascript in the browser: DOM Manipulation and Event Handling</p>
+                            <p>DRY principle</p>
                         </div>
-                        <a href="#" target="_blank" className="btn btn--white">Visit us</a>
+                        <a href="#" target="_blank" className="btn btn--white">Let's play?</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="card__side card__side--front">
-                        <div className="card__picture card__picture-1">
+                        <div className="card__picture card__picture-budgety">
+                            <h4 className="card__heading">Budget App</h4>
+                        </div>
+                    </div>
+                    <div className="card__side card__side--back card__side--back-2">
+                        <div>
+                            <p>The Budget App it's a fictional application to manage our personal finance life</p>
+                            <p>Introduction to Module Pattern, Encapsulation and Event Delegation </p>
+                            <p>How to read data from different HTML input types</p>
+                        </div>
+                        <a href="#" target="_blank" className="btn btn--white">Check it out</a>
+                    </div>
+                </div>
+                <div className="card">
+                    <div className="card__side card__side--front">
+                        <div className="card__picture card__picture-forkify">
                             <h4 className="card__heading">Forkify</h4>
                         </div>
                     </div>
                     <div className="card__side card__side--back card__side--back-1">
                         <div>
-                            <p>E-commerce platform specialized in bathroom accessories; </p>
-                            <p>Build first version oh the platform using HTML, CSS, jQuery and Bootstrap;</p>
-                            <p>Member of the react team that implemented project with react</p>
-                            <p>Setup continuous</p>
+                            <p>Forkify is a food recipe application build using food2fork API </p>
+                            <p>Introduction to MCV Architecture, ES6 Modules and Classes, Local Storage, Error Handling, Application State and REST-API</p>
                         </div>
-                        <a href="#" target="_blank" className="btn btn--white">Visit us</a>
+                        <a href="#" target="_blank" className="btn btn--white">Check it out</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="card__side card__side--front">
-                        <div className="card__picture card__picture-1">
+                        <div className="card__picture card__picture-natours">
                             <h4 className="card__heading">Natours</h4>
                         </div>
                     </div>
                     <div className="card__side card__side--back card__side--back-1">
                         <div>
-                            <p>E-commerce platform specialized in bathroom accessories; </p>
-                            <p>Build first version oh the platform using HTML, CSS, jQuery and Bootstrap;</p>
-                            <p>Member of the react team that implemented project with react</p>
-                            <p>Setup continuous</p>
+                            <p>Introduction to SASS: Variables, Nesting, Mixins, Extends, Functions and The 7-1 Pattern</p>
+                            <p>Advanced CSS: Skew, object-fit, translate, perspective, clip-path, outline-offset, filter ... </p>
+                            <p>{`New HTML tags and attributes: <picture> <img srcset=" "> <video>`}</p>
                         </div>
-                        <a href="#" target="_blank" className="btn btn--white">Visit us</a>
+                        <a href="#" target="_blank" className="btn btn--white">Check it out</a>
                     </div>
                 </div>
                 <div className="card">
                     <div className="card__side card__side--front">
-                        <div className="card__picture card__picture-1">
-                            <h4 className="card__heading">Nexter</h4>
-                        </div>
-                    </div>
-                    <div className="card__side card__side--back card__side--back-1">
-                        <div>
-                            <p>E-commerce platform specialized in bathroom accessories; </p>
-                            <p>Build first version oh the platform using HTML, CSS, jQuery and Bootstrap;</p>
-                            <p>Member of the react team that implemented project with react</p>
-                            <p>Setup continuous</p>
-                        </div>
-                        <a href="#" target="_blank" className="btn btn--white">Visit us</a>
-                    </div>
-                </div>
-                <div className="card">
-                    <div className="card__side card__side--front">
-                        <div className="card__picture card__picture-1">
+                        <div className="card__picture card__picture-trillo">
                             <h4 className="card__heading">Trillo</h4>
                         </div>
                     </div>
                     <div className="card__side card__side--back card__side--back-1">
                         <div>
-                            <p>E-commerce platform specialized in bathroom accessories; </p>
-                            <p>Build first version oh the platform using HTML, CSS, jQuery and Bootstrap;</p>
-                            <p>Member of the react team that implemented project with react</p>
-                            <p>Setup continuous</p>
+                            <p>Introduction to Flexbox</p>
+                            <p>Advanced Flexbox properties for positioning and alignment</p>
+                            <p>SVG icons Vs font icons</p>
                         </div>
-                        <a href="#" target="_blank" className="btn btn--white">Visit us</a>
+                        <a href="#" target="_blank" className="btn btn--white">Check it out</a>
                     </div>
                 </div>
             </div>
@@ -165,7 +146,5 @@ const PortfolioPage = () => (
         <div className="white-bar-to-continue-reading"></div>
     </div>
 );
-
-
 
 export default PortfolioPage;
